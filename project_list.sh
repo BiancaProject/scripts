b801e1092bbcb5b4e232ff32d795493fac4ba22f
alanndz <alanndz7@gmail.com>
2022-04-24 17:14:00 +0000

    Add project list
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- project_list.sh
+++ project_list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2022 Bianca Project
+# Copyright (C) 2020-2022 Bianca Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,30 +13,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 if [ ! -e "build/envsetup.sh" ]; then
     echo "Must run from root of repo"
     exit 1
 fi
 
-usage() {
-  echo "Usage ${0} <merger|save|backup|branch|rebase|restore|reset|checkout|push> <argument>"
-  exit 1
-}
+TOP="${PWD}"
+LIST="${TOP}/project.list"
+BLACKLIST=$(cat "${TOP}/scripts/blacklist")
+MANIFEST="${TOP}/.repo/manifests/snippets/bianca.xml"
 
-ARG="$1"
-TOP=$(pwd)
-SC="${TOP}/scripts"
+# Build list of Bianca Project forked repos
+PROJECTPATHS=$(grep "remote=\"dudu" "${MANIFEST}" | sed -n 's/.*path="\([^"]\+\)".*/\1/p')
 
-case "${ARG}" in
-  merger)
-    "${SC}/aosp-merger.sh" "${@:2}" ;;
-  save|branch)
-    "${SC}/branches_${ARG}.sh" ;;
-  backup|rebase|restore|reset|checkout|push)
-    "${SC}/branches_${ARG}.sh" "${@:2}" ;;
-  list)
-    "${SC}/project_${ARG}.sh" ;;
-  *)
-    usage ;;
-esac
+for PROJECTPATH in ${PROJECTPATHS}; do
+    if [[ "${BLACKLIST}" =~ "${PROJECTPATH}" ]]; then
+        continue
+    fi
+
+    echo "${PROJECTPATH}"
+done | sort > "${LIST}"
+
